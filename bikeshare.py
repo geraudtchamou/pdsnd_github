7beadd0208f2edd05563cea9116d97fb545ad472
geraudtchamou <geraudtchamouw@gmail.com>
2020-04-17 10:16:11 +0100

    some improment on my code

--- bikeshare.py
+++ bikeshare.py
@@ -71,10 +71,10 @@ def load_data(city, month, day):
         df - Pandas DataFrame containing city data filtered by month and day
     """
 
-    # load data file into a dataframe
+    # loading data file into a dataframe
     df = pd.read_csv(CITY_DATA[city])
 
-    # convert the Start Time column to datetime
+    # converting the Start Time column to datetime
     df['Start Time'] = pd.to_datetime(df['Start Time'])
 
     # extract month and day of week from Start Time to create new columns
